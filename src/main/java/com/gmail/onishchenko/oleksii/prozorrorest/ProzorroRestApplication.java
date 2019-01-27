0e7dc4298b890dd5fafa900a0e4042bf0f28a91a
JIeIIIa <stealth@bigmir.net>
2019-01-27 10:12:44 +0200

    Initial commit

---
+++ src/main/java/com/gmail/onishchenko/oleksii/prozorrorest/ProzorroRestApplication.java
@@ -0,0 +1,14 @@
+package com.gmail.onishchenko.oleksii.prozorrorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProzorroRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProzorroRestApplication.class, args);
+    }
+
+}
+
